065319dc970420f60b67ac1a9a17ae5fd9dc22bd
MonikaSingh9219 <monika.singh9219@gmail.com>
2022-08-16 22:32:34 +0530

    changes done!!!!

--- JenkinsPractice_Framework/src/test/java/com/cast/jenkinsdemo1/CreateOrgTest.java
+++ JenkinsPractice_Framework/src/test/java/com/cast/jenkinsdemo1/CreateOrgTest.java
@@ -7,21 +7,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import com.mysql.cj.jdbc.Driver;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CreateOrgTest {
 	@Test 
 	public void createOrgTest() throws Throwable 
 	{
-		WebDriverManager.chromedriver().setup(); 
-		WebDriver driver = new ChromeDriver();
-		FileInputStream fis = new FileInputStream("./Data/CommonProperty.properties");
-		Properties p = new Properties();
+	WebDriverManager.chromedriver().setup(); 
+	WebDriver driver = new ChromeDriver();
+//		FileInputStream fis = new FileInputStream("./Data/CommonProperty.properties");
+//		Properties p = new Properties();
+//		
+//		p.load(fis);
 		
-		p.load(fis);
-		
-		String Url=p.getProperty("url");
-		driver.get(Url);
+		String URL=System.getProperty("url");
+		System.out.println(URL);
+		driver.get(URL);
 	System.out.println("Organization Created1111");
 }
 }
